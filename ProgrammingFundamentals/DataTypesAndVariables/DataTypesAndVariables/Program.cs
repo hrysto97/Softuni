effb76fec9eec789d804fbdd83835f46efd70913
Hristo Dinkov <hrysto97@gmail.com>
2017-06-01 19:04:16 +0300

    Solved some problems

--- ProgrammingFundamentals/DataTypesAndVariables/DataTypesAndVariables/Program.cs
+++ ProgrammingFundamentals/DataTypesAndVariables/DataTypesAndVariables/Program.cs
@@ -8,8 +8,23 @@ namespace DataTypesAndVariables
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            sbyte a = -100;
+            byte b = 128;
+            int c = -3540;
+            int d = 64876;
+            uint l = 2147483648;
+            int k = -1141583228;
+            long s = -1223372036854775808;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(l);
+            Console.WriteLine(k);
+            Console.WriteLine(s);
         }
     }
 }
